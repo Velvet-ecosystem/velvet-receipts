cf394508284e89023e8acf6917dee4f2c38e2edb
Overlandranger <36721943+Overlandranger@users.noreply.github.com>
2026-08-10 15:53:15 -0700

    Export audio output receipt family

--- velvet_receipts/__init__.py
+++ velvet_receipts/__init__.py
@@ -4,6 +4,13 @@
 from receipt import Receipt
 from receipt_logger import GENESIS_HASH, ReceiptLogger
 from runtime_receipts import runtime_receipt_from_envelope
+from audio_output_receipts import (
+    AUDIO_OUTPUT_EVENT_CONTRACT,
+    AUDIO_OUTPUT_RECEIPT_EVENTS,
+    AUDIO_OUTPUT_RECEIPT_SCHEMA,
+    AudioOutputReceiptError,
+    audio_output_receipt_from_envelope,
+)
 from ghost_can_receipts import (
     GHOST_CAN_OBSERVATION_EVENT,
     GHOST_CAN_RECEIPT_SCHEMA,
@@ -20,6 +27,11 @@ __all__ = [
     "GENESIS_HASH",
     "Receipt",
     "ReceiptLogger",
+    "AUDIO_OUTPUT_EVENT_CONTRACT",
+    "AUDIO_OUTPUT_RECEIPT_EVENTS",
+    "AUDIO_OUTPUT_RECEIPT_SCHEMA",
+    "AudioOutputReceiptError",
+    "audio_output_receipt_from_envelope",
     "GHOST_CAN_OBSERVATION_EVENT",
     "GHOST_CAN_RECEIPT_SCHEMA",
     "ghost_can_receipt_from_envelope",
